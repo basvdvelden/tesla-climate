dd51a9bade38e8a1d75d1197945a1d384573c299
basvdvelden <43495835+basvdvelden@users.noreply.github.com>
2025-01-27 09:36:37 +0100

    Update AuthBody.java

--- app/src/main/java/nl/vandervelden/teslaclimate/api/AuthBody.java
+++ app/src/main/java/nl/vandervelden/teslaclimate/api/AuthBody.java
@@ -2,7 +2,7 @@ package nl.vandervelden.teslaclimate.api;
 
 public class AuthBody {
     private String grant_type = "refresh_token";
-    private String client_secret = "c7257eb71a564034f9419ee651c7d0e5f7aa6bfbd18bafb5c5c033b093bb2fa3";
+    private String client_secret = "secret";
     private String client_id = "81527cff06843c8634fdc09e8ac0abefb46ac849f38fe1e431c2ef2106796384";
     private String refresh_token;
 
